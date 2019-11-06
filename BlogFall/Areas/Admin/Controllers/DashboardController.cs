a94df03da9afc91de1b1b275f576f985e3e5d3be
medine ozkanli <ozknlimedine@gmail.com>
2019-11-06 21:26:54 +0300

    Adminde düzenleme ve silme işlemleri yapıldı

--- BlogFall/Areas/Admin/Controllers/DashboardController.cs
+++ BlogFall/Areas/Admin/Controllers/DashboardController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace BlogFall.Areas.Admin.Controllers
 {
-    public class DashboardController : Controller
+    public class DashboardController : AdminBaseController
     {
   
 
